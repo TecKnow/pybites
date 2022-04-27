495330d79d8d43201b8a8973ba7c84a1478a4faf
David P. Perkins <tecknow@grumbleware.com>
2022-04-27 14:22:41 -0500

    submission Bite 138 @ codechalleng.es

---
+++ 138/zoo.py
@@ -0,0 +1,20 @@
+from weakref import WeakSet
+
+
+class Animal:
+
+    def __init__(self, name: str):
+        self.name: str = name
+        self.__class__.serial_counter += 1
+        self.serial_number: int = self.__class__.serial_counter
+        self.__class__.instance_references.add(self)
+
+    def __str__(self):
+        return f"{self.serial_number}. {self.name.title()}"
+
+    @classmethod
+    def zoo(cls):
+        return '\n'.join(tuple(str(r) for r in sorted(cls.instance_references, key=lambda x: x.serial_number)))
+
+    serial_counter: int = 10000
+    instance_references: set['Animal'] = set()
