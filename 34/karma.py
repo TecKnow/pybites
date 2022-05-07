90c029b6d971c9f7849d003216b1700f28b0c0c2
David P. Perkins <tecknow@grumbleware.com>
2022-05-07 12:13:40 -0500

    submission Bite 34 @ codechalleng.es

---
+++ 34/karma.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from datetime import datetime
+
+Transaction = namedtuple(
+    'Transaction',
+    'giver points date',
+    defaults=(None, None, datetime.now()))
+
+
+class User:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._transactions = list()
+
+    @property
+    def fans(self):
+        return len({x.giver for x in self._transactions})
+    
+    @property
+    def points(self):
+        return [x.points for x in self._transactions]
+    
+    @property
+    def karma(self):
+        return sum(self.points)
+    
+    def __add__(self, _o: Transaction):
+        self._transactions.append(_o)
+    
+    def __str__(self) -> str:
+        return f"{self.name} has a karma of {self.karma} and {self.fans} fan{'s' if self.fans !=1 else ''}"
