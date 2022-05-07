06fa59777b38c881e7ba4bcb874c3a21000374dd
David P. Perkins <tecknow@grumbleware.com>
2022-05-07 12:15:00 -0500

    submission Bite 34 @ codechalleng.es

--- 34/karma.py
+++ 34/karma.py
@@ -15,17 +15,17 @@ class User:
     @property
     def fans(self):
         return len({x.giver for x in self._transactions})
-    
+
     @property
     def points(self):
         return [x.points for x in self._transactions]
-    
+
     @property
     def karma(self):
         return sum(self.points)
-    
+
     def __add__(self, _o: Transaction):
         self._transactions.append(_o)
-    
+
     def __str__(self) -> str:
         return f"{self.name} has a karma of {self.karma} and {self.fans} fan{'s' if self.fans !=1 else ''}"
