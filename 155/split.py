959d4520692bb115811d09daafa71783f1e86c98
David P. Perkins <tecknow@grumbleware.com>
2022-07-05 00:07:19 -0500

    submission Bite 155 @ codechalleng.es

---
+++ 155/split.py
@@ -0,0 +1,17 @@
+import shlex
+from typing import List
+
+
+def split_words_and_quoted_text(text: str) -> List[str]:
+    """Split string text by space unless it is
+       wrapped inside double quotes, returning a list
+       of the elements.
+
+       For example
+       if text =
+       'Should give "3 elements only"'
+
+       the resulting list would be:
+       ['Should', 'give', '3 elements only']
+    """
+    return shlex.split(text)
