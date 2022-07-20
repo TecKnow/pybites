847eaa04f7bdd04b94d36307a19de27ab2fc20d4
David P. Perkins <tecknow@grumbleware.com>
2022-07-20 17:06:12 -0500

    submission Bite 49 @ codechalleng.es

--- 49/packt.py
+++ 49/packt.py
@@ -5,19 +5,30 @@ import requests
 
 PACKT = 'https://bites-data.s3.us-east-2.amazonaws.com/packt.html'
 CONTENT = requests.get(PACKT).text
-FREE_BANNER_ID = "free-learning-banner"
+DOTD_MAIN_CLASS = "dotd-main-book"
+DOTD_SUMMARY_CLASS = "dotd-main-book-summary"
+DOTD_IMAGE_CLASS = "dotd-main-book-image"
 
 Book = namedtuple('Book', 'title description image link')
 
 
 def get_book():
+    from pprint import pprint
     """make a Soup object, parse the relevant html sections, and return a Book namedtuple"""
     soup = Soup(CONTENT, 'html.parser')
     # print(soup.prettify())
-    free_banner = soup.find(id=FREE_BANNER_ID)
-    free_area = free_banner.parent
-    free_title = free_area.find(class_="dotd-title")
-    print((free_title.text.strip()))
+    free_area = soup.find(class_=DOTD_MAIN_CLASS)
+    free_title = free_area.find(class_="dotd-title").text.strip()
+    description_area = free_area.find(class_=DOTD_SUMMARY_CLASS)
+    description_div = description_area.find(
+        "div", id=lambda attr: not attr, class_=lambda attr: not attr)
+    description_string = description_div.text.strip()
+    image_div = free_area.find(class_=DOTD_IMAGE_CLASS)
+    book_link = image_div.find('a')
+    book_url = book_link.get("href").strip()
+    image_tag = book_link.find('img')
+    image_url = image_tag.get('src').strip()
+    return Book(free_title, description_string, image_url, book_url)
 
 
 if __name__ == "__main__":
