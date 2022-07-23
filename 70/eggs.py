5bc01f788555e62a8bb2702b1aa851fe80f8e271
David P. Perkins <tecknow@grumbleware.com>
2022-07-23 12:11:48 -0500

    submission Bite 70 @ codechalleng.es

---
+++ 70/eggs.py
@@ -0,0 +1,20 @@
+from typing import Iterator
+from random import choice
+
+COLORS = 'red blue green yellow brown purple'.split()
+
+
+class EggCreator:
+    def __init__(self, limit: int) -> None:
+        self.limit = limit
+        self.current = 0
+
+    def __iter__(self) -> Iterator[str]:
+        return self
+
+    def __next__(self) -> str:
+        if self.current < self.limit:
+            self.current += 1
+            return choice(COLORS)
+        else:
+            raise StopIteration()
