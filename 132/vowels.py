50d4d9acbb5a987c881b5899de9fb3fce42f15ef
David P. Perkins <tecknow@grumbleware.com>
2022-08-07 16:24:15 -0500

    Make the solution for byte 132 case insensitive.

--- 132/vowels.py
+++ 132/vowels.py
@@ -1,10 +1,14 @@
 from typing import Tuple
 
-VOWELS = set('aeiou')
+VOWELS = set('aeiou'.casefold())
 
 
 def get_word_max_vowels(text: str) -> Tuple[str, int]:
     """Get the case insensitive word in text that has most vowels.
        Return a tuple of the matching word and the vowel count, e.g.
        ('object-oriented', 6)"""
-    return max(((word, len([char for char in word if char in VOWELS])) for word in text.split()), key=lambda x: x[1])
+    return max(
+        ((word, len([char for char in word if char in VOWELS]))
+         for word in text.casefold().split()),
+        key=lambda x: x[1]
+    )
