fbfbc85065bb5aecac681b07be67c5b6cef8d567
David P. Perkins <tecknow@grumbleware.com>
2022-08-18 15:53:49 -0500

    submission Bite 100 @ codechalleng.es

---
+++ 100/tail.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from typing import List
+
+
+def tail(filepath: Path, n: int) -> List[str]:
+    """
+    Similate Unix' "tail -n" command:
+    - Read in the file ("filepath").
+    - Parse it into a list of lines, stripping trailing newlines.
+    - Return the last "n" lines.
+    """
+    return [line.strip() for line in filepath.read_text().strip().splitlines()][-n:]
