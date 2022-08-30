c049c1c2122ae2a06a3001906bcc6c19b7682f8f
David P. Perkins <tecknow@grumbleware.com>
2022-08-30 16:19:20 -0500

    submission Bite 143 @ codechalleng.es\nUpdated for brevity and type checking.

--- 143/merge.py
+++ 143/merge.py
@@ -5,15 +5,12 @@ group1 = {'tim': 30, 'bob': 17, 'ana': 24}
 group2 = {'ana': 26, 'thomas': 64, 'helen': 26}
 group3 = {'brenda': 17, 'otto': 44, 'thomas': 46}
 
-all_names: ChainMap[str, int]= ChainMap(group3, group2, group1)
+all_names: ChainMap[str, int] = ChainMap(group3, group2, group1)
+
 
 def get_person_age(name: str) -> Union[str, int]:
     """Look up name (case insensitive search) and return age.
        If name in > 1 dict, return the match of the group with
        greatest N (so group3 > group2 > group1)
     """
-    age = all_names.get(str(name))
-    if age is None:
-      return NOT_FOUND
-    return age
-
+    return age if (age := all_names.get(str(name).lower())) is not None else NOT_FOUND
