8ea47646878857d1f7ff5e439b9038d440372cfa
David P. Perkins <tecknow@grumbleware.com>
2022-09-13 20:54:16 -0500

    submission Bite 131 @ codechalleng.es

---
+++ 131/screenfetch.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+output = """
+                                       mohh@SERENiTY
+ MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        OS: Mint 19 tara
+ MMm----::-://////////////oymNMd+'     Kernel: x86_64 Linux 4.15.0-34-generic
+ MMd      /++                -sNMd:    Uptime: 1d 4m
+ MMNso/'  dMM    '.::-. .-::.' .hMN:   Packages: 2351
+ ddddMMh  dMM   :hNMNMNhNMNMNh: 'NMm   Shell: zsh 5.4.2
+     NMm  dMM  .NMN/-+MMM+-/NMN' dMM   Resolution: 1366x768
+     NMm  dMM  -MMm  'MMM   dMM. dMM   DE: Cinnamon 3.8.9
+     NMm  dMM  -MMm  'MMM   dMM. dMM   WM: Muffin
+     NMm  dMM  .mmd  'mmm   yMM. dMM   WM Theme: Linux Mint (Mint-Y)
+     NMm  dMM'  ..'   ...   ydm. dMM   GTK Theme: Mint-Y [GTK2/3]
+     hMM- +MMd/-------...-:sdds  dMM   Icon Theme: Mint-Y
+     -NMm- :hNMNNNmdddddddddy/'  dMM   Font: Noto Sans 9
+      -dMNs-''-::::-------.''    dMM   CPU: AMD A10-7400P Radeon R6, 10 Compute Cores 4C+6G @ 4x 2.5GHz [101.0°C]
+       '/dMNmy+/:-------------:/yMMM   GPU: AMD KAVERI (DRM 2.50.0 / 4.15.0-34-generic, LLVM 6.0.0)
+          ./ydNMMMMMMMMMMMMMMMMMMMMM   RAM: 1886MiB / 6915MiB
+             \.MMMMMMMMMMMMMMMMMMM    
+"""
+
+
+def sysinfo_scrape(output: str) -> Dict[str, str]:
+    """Scrapes the output from screenfetch and returns a dictionary"""
+    lines_without_logo = [ rline for line in output.splitlines() if (rline:=line[line.rfind("  "):].strip())]
+    user_name, *data_lines = lines_without_logo
+    data_lines.insert(0, f"Name: {user_name}")
+    return dict(((key.strip(), value.strip()) for key, value in (data_line.split(":", 1) for data_line in data_lines)))
+
+if __name__== "__main__":
+    from pprint import pprint 
+    pprint(sysinfo_scrape(output))
