4484126548de042f35413ceeb51571af5e27714c
David P. Perkins <tecknow@grumbleware.com>
2022-09-15 13:43:22 -0500

    submission Bite 2 @ codechalleng.es

---
+++ 2/regex.py
@@ -0,0 +1,48 @@
+import re
+
+COURSE = ('Introduction 1 Lecture 01:47'
+          'The Basics 4 Lectures 32:03'
+          'Getting Technical!  4 Lectures 41:51'
+          'Challenge 2 Lectures 27:48'
+          'Afterword 1 Lecture 05:02')
+TWEET = ('New PyBites article: Module of the Week - Requests-cache '
+         'for Repeated API Calls - http://pybit.es/requests-cache.html '
+         '#python #APIs')
+HTML = ('<p>pybites != greedy</p>'
+        '<p>not the same can be said REgarding ...</p>')
+
+
+def extract_course_times(course=COURSE):
+    """Return the course timings from the passed in
+       course string. Timings are in mm:ss (minutes:seconds)
+       format, so taking COURSE above you would extract:
+       ['01:47', '32:03', '41:51', '27:48', '05:02']
+       Return this list.
+    """
+    course_time_re = re.compile(r"\d\d:\d\d")
+    return [m.group() for m in course_time_re.finditer(course)]
+
+
+def get_all_hashtags_and_links(tweet=TWEET):
+    """Get all hashtags and links from the tweet text
+       that is passed into this function. So for TWEET
+       above you need to extract the following list:
+       ['http://pybit.es/requests-cache.html',
+        '#python',
+        '#APIs']
+       Return this list.
+    """
+    tweet_re = re.compile(
+        r"((?P<hashtag>#\w*)|(?:\s)(?P<url>\S+://\S+)(?:\s*))")
+    return [m.group('hashtag') or m.group('url') for m in tweet_re.finditer(tweet)]
+
+
+def match_first_paragraph(html=HTML):
+    """Extract the first paragraph of the passed in
+       html, so for HTML above this would be:
+       'pybites != greedy' (= content of first paragraph).
+       Return this string.
+    """
+    html_re = re.compile(r"<p>(?P<ptext>.*?)</p>")
+    for m in html_re.finditer(html):
+        return m.group("ptext")
