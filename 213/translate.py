871ddabfaaca03336dd9d054792cdde7992eb639
David P. Perkins <tecknow@grumbleware.com>
2022-09-22 11:36:46 -0500

    submission Bite 213 @ codechalleng.es

--- 213/translate.py
+++ 213/translate.py
@@ -9,19 +9,9 @@ def fix_translation(org_text: str, trans_text: str) -> str:
        Parse trans_text restoring the original (English) code (wrapped inside
        code and pre tags) into it. Return the fixed translation str
     """
-    res = str()
     untranslated_elements = non_translate_re.split(org_text)
     translated_elements = non_translate_re.split(trans_text)
     assert len(untranslated_elements) == len(translated_elements)
-    for org, trans in zip(untranslated_elements, translated_elements):
-        if org.startswith(("<pre>", "<code>")):
-            res += org
-        else:
-            res += trans
-    return res
-
-
-if __name__ == "__main__":
-    from pprint import pprint
-    from test_translate import bite_15_en
-    pprint(non_translate_re.split(bite_15_en))
+    res_list = list(translated_elements)
+    res_list[1::2] = untranslated_elements[1::2]
+    return ''.join(res_list)
