dac4ca377fe9af37d23be7195bf97868ff515a0d
David P. Perkins <tecknow@grumbleware.com>
2022-10-07 16:21:15 -0500

    submission Bite 209 @ codechalleng.es

---
+++ 209/docstring.py
@@ -0,0 +1,10 @@
+def sum_numbers(numbers):
+    """Sums numbers
+
+    :param numbers: a lit of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
