9cd8589ec30b3ad94bda3d31894ffbbdd7ebce93
David P. Perkins <tecknow@grumbleware.com>
2023-06-04 12:01:20 -0500

    Update solution for bite 362

--- 362/script.py
+++ 362/script.py
@@ -6,7 +6,7 @@ app = typer.Typer()
 
 @app.command()
 def main(username: str,
-    passwd: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)]):
+    passwd: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)] = ""):
     print(  f"Hello {username}. Doing something very secure with password.\n"
             f"...just kidding, here it is, very insecure: {passwd}\n",)
 
