16bcbb3d02c73c48cf05f09f85afb65a598ec927
David P. Perkins <tecknow@grumbleware.com>
2023-06-05 16:03:45 -0500

    Update solution for bite 362
    Pybites is using an older version (0.7.0?) of typer which does not
    support annotations.

--- 362/script.py
+++ 362/script.py
@@ -1,15 +1,18 @@
 import typer
-from typing_extensions import Annotated
 
 app = typer.Typer()
 
 
 @app.command()
-def main(username: str,
-    passwd: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)] = ""):
-    print(  f"Hello {username}. Doing something very secure with password.\n"
-            f"...just kidding, here it is, very insecure: {passwd}\n",)
-
+def main(
+    name: str,
+    password: str = typer.Option(
+        ..., prompt=True, confirmation_prompt=True, hide_input=True
+    ),
+):
+    print(f"Hello {name}. Doing something very secure with password.")
+    print(f"...just kidding, here it is, very insecure: {password}")
+    
 
 
 if __name__ == "__main__":
